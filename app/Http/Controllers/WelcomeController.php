dbcdb6c86b91796a0d371ead9c95948f463adb35
王亮平 <806446104@qq.com>
2015-09-12 13:49:01 +0800

    架构一次调整

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers;
+/**
+ * Created by PhpStorm.
+ * User: bluse
+ * Date: 15/9/12
+ * Time: 下午12:20
+ */
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        echo "hello world";
+    }
+}
