473cb607376067688c8b52a70f08949d57803efd
王亮平 <806446104@qq.com>
2015-09-17 18:10:41 +0800

    架构二次调整

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,6 +9,16 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $obj = (object)'123';
+        echo $obj->scalar;
+        $arr = [];
+        $arr['key'] = 'website';
+        $arr['value'] = 'www.baidu.com';
+        $a = serialize($arr);
+        echo $a.'\n';
+        $a = unserialize($a);
+        dd($a);
+        echo (int)((0.1 + 0.7)*10);
         echo "hello world";
     }
 }
